f7908b64573675da1cda570b79b4ec8dc2380a13
xu zhuoqing <843486783@qq.com>
2022-03-09 19:58:59 +0800

    init project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  console.log("eeeee")
   return <Component {...pageProps} />
 }
 
