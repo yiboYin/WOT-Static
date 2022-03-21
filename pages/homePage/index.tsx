2231cc554eeb2059f2187e8243bd3f7f0a783975
yiboYin <1535213303@qq.com>
2022-03-21 19:20:08 +0800

    [Feature] use styled-components in homepage

--- pages/homePage/index.tsx
+++ pages/homePage/index.tsx
@@ -1,20 +1,44 @@
-import { useState } from 'react';
+import { useState } from 'react'
 import type { NextPage } from 'next'
-import { Button, Select, Input } from 'antd';
+import { Button, Select, Input } from 'antd'
 
-import styles from '../../styles/homepage.module.css'
+import styled from 'styled-components'
+// import bg from '../../assets/img/homepage-bg.jpg'
 
 const HomePage: NextPage = () => {
-  const INIT_REGION = 'RU';
-  const SERVER_REGION = ['RU', 'EU', 'ASIA', 'NA'];
-  const { Option } = Select;
+  const INIT_REGION = 'RU'
+  const SERVER_REGION = ['RU', 'EU', 'ASIA', 'NA']
+  const { Option } = Select
 
-  const [region, setRegion] = useState(INIT_REGION);
-  const [accountName, setAccountName] = useState('');
+  const [region, setRegion] = useState<string>(INIT_REGION)
+  const [accountName, setAccountName] = useState<string>('')
 
-  const inputChangeHandler = (e:any) => {
-    const { target: { value = '' } = {} } = e;
-    setAccountName(value);
+  const Wrapper = styled.div`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-image: ${require('../../assets/img/homepage-bg.jpg')};
+    background-size: 100%;
+  `
+
+  const InputContainer = styled.div`
+    width: 60vw;
+    display: flex;
+    > .serverSelector div {
+      background-color: #ff850a !important;
+      color: white;
+    }
+    > .searchBtn {
+      background-color: #ff850a !important;
+      color: white;
+    }
+  `
+
+  const inputChangeHandler = (e: React.SyntheticEvent) => {
+    const value = (e.target as HTMLInputElement).value
+    setAccountName(value)
   }
 
   const submitHandler = () => {
@@ -22,20 +46,24 @@ const HomePage: NextPage = () => {
   }
 
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.inputContainer}>
+    <Wrapper>
+      <InputContainer>
         {/* <div>title & logo</div> */}
-        <Select size='large' className={styles.serverSelector} value={region} onSelect={(v:string) => setRegion(v)}>
-          {
-            SERVER_REGION.map(each => {
-              return (<Option value={each} key={each}>{each}</Option>)
-            })
-          }
+        <Select size="large" className="serverSelector" value={region} onSelect={(v: string) => setRegion(v)}>
+          {SERVER_REGION.map((each) => {
+            return (
+              <Option value={each} key={each}>
+                {each}
+              </Option>
+            )
+          })}
         </Select>
         <Input size="large" value={accountName} onChange={inputChangeHandler} />
-        <Button size='large' type="primary" className={styles.searchBtn} onClick={submitHandler}>Submit</Button>
-      </div>
-    </div>
+        <Button size="large" type="primary" className="searchBtn" onClick={submitHandler}>
+          Submit
+        </Button>
+      </InputContainer>
+    </Wrapper>
   )
 }
 
